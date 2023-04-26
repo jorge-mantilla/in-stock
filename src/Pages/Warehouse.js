f4b51d95ac70db1ec7374d3da94e3bc0af2d1248
aborgono <albertoiborgonovo@gmail.com>
2023-04-25 20:33:59 -0400

    added function and mapping to warehouses component // imported useState on page

--- src/Pages/Warehouse.js
+++ src/Pages/Warehouse.js
@@ -2,11 +2,7 @@ import Warehouses from "../components/Warehouses/warehouses";
 import { useEffect } from "react";
 
 function WarehousePage() {
-    useEffect(() => {
-        axios.get(`http://localhost:8080/videos`).then((response) => {
-        setVideosArray(response.data)
-        })
-    }, []);
+    
     return (
     <>
     <h1>404</h1>
--- src/components/Warehouses/warehouses.js
+++ src/components/Warehouses/warehouses.js
@@ -1,8 +1,28 @@
+import axios from "axios";
+import { useState } from "react";
+
 function Warehouses() {
+    const [warehousesArray, setWarehousesArray] = useState([])
+
+    useEffect(() => {
+        axios.get(``).then((response) => {
+        setWarehousesArray(response.data)
+        })
+    }, []);
     return (
     <>
-    <h1>404</h1>
-    <p>Can't find what you're looking for, sorry!</p>
+        <div>
+            {warehousesArray.map((warehouse) => {
+                <ul>
+                    <li>{warehouse.warehouse}</li>
+                    <li>{warehouse.address}</li>
+                    <li>{warehouse.contactName}</li>
+                    <li>{warehouse.contactInformation}</li>
+                    <li>ACTIONS</li>
+                </ul>
+            })
+            }
+        </div>
     </>
 );
 }
