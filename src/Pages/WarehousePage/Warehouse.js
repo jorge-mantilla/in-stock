7be642e898f95ae78781412bf72d0432f44e6bd4
aborgono <albertoiborgonovo@gmail.com>
2023-04-27 10:43:46 -0400

    completed function to get all warehouses and  fixed errors in console

--- src/Pages/WarehousePage/Warehouse.js
+++ src/Pages/WarehousePage/Warehouse.js
@@ -1,5 +1,7 @@
 import Warehouses from "../../components/Warehouses/Warehouses";
 import { useEffect } from "react";
+import { Link } from 'react-router-dom';
+
 
 function WarehousePage() {
     
@@ -9,7 +11,7 @@ function WarehousePage() {
             <h1>WAREHOUSES</h1>
             <form id="upload__form-input" className="upload__form-input" novalidate>
                 <div className="upload__div">
-                    <label className="upload__name__type" for="title">TITLE YOUR VIDEO</label>
+                    {/* <label className="upload__name__type" for="title">TITLE YOUR VIDEO</label> */}
                     <input className="upload__name__text" type="text" name="title" placeholder="Search..." required />
                 </div>
                 <div>
