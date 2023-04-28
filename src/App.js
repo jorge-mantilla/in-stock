d6d37f66149b24e31226fd9b15ef169a43081910
Brenda G <bren.gonzalez1926@gmail.com>
2023-04-28 03:16:03 -0400

    major | Completed Warehouse Page, Header, Footer, and Reactive for all

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import './App.css';
-import Main from './components/Main/Main';
 import WarehousePage from './Pages/WarehousePage/Warehouse';
+import NotFound from './components/NotFound/NotFound';
+import Main from './components/Main/Main';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import WarehouseDetailsPage from './Pages/WarehouseDetailsPage/WarehouseDetails';
 import InventoryPage from './Pages/InventoryPage/Inventory';
-import InventoryListPage from './Pages/InventoryDetails/InventoryDetails';
-import NotFound from './components/NotFound/NotFound';
 import { Routes, Route } from 'react-router-dom';
+import './App.scss';
 //installed axios
 // npm i axios dotenv react-com react-router-dom scss
 //git repo check
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element={<Main/>}></Route>
-        <Route path='/Warehouse' element={<WarehousePage />}></Route>
-        <Route path='/WarehouseDetails' element={<WarehouseDetailsPage />}></Route>
-        <Route path='/Inventory' element={<InventoryPage />}></Route>
-        <Route path='/InventoryDetails' element={<InventoryListPage />}></Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <div className='header__bg'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Main />}></Route>
+            <Route path='/warehouses' element={<WarehousePage />}></Route>
+            <Route path='/warehouseDetails' element={<WarehouseDetailsPage />}></Route>
+            <Route path='/inventory' element={<InventoryPage />}></Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </div>
+      </div >
+    </>
   );
 }
 
