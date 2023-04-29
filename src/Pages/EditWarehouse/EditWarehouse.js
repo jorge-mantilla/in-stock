91ba1ac945a22642677db8b3d8db53f511d27734
Jorge Mantilla <33700227+manto465@users.noreply.github.com>
2023-04-28 23:12:32 -0400

    tablet and desktop view complete for edit and add

--- src/Pages/EditWarehouse/EditWarehouse.js
+++ src/Pages/EditWarehouse/EditWarehouse.js
@@ -42,114 +42,125 @@ function EditWarehouse() {
   };
 
   return (
-    <section className="addForm__container">
-      <article className="addForm">
-        <h1 className="addForm__header">Edit Warehouse</h1>
-        <Link to='/warehouses'>
-        <img id='warehouse__btn--icon' className="btn--icon" src={AddIcon} alt="Add Icon" />
-        </Link>
+    <article className="addForm">
+      <div className="addForm__tablet-header-box">
+      <h1 className="addForm__header">Edit Warehouse</h1>
+      <Link to="../warehouses">
+        <img
+          id="warehouse__btn--icon"
+          className="btn--icon"
+          src={AddIcon}
+          alt="Add Icon"
+        />
+      </Link>
+      </div>
+      <div className="addForm__tablet-wrapper">
+      <div className="addForm__tablet-form-box">
+      <div className="addForm__whd-border"></div>
+      <div className="addForm__whd-container">
+        <h2 className="addForm__subheader">Warehouse Details</h2>
+
+        <form className="addForm__wh-form" onSubmit={handleSubmit} noValidate>
+          <label className="addForm__label">Warehouse Name</label>
+          <input
+            type="text"
+            name="warehouseName"
+            placeholder="Warehouse Name"
+            value={formData.warehouseName}
+            onChange={handleInputChange}
+            className="addForm__input"
+          />
+
+          <label className="addForm__label">Street Address</label>
+          <input
+            type="text"
+            name="streetAddress"
+            placeholder="Street Address"
+            value={formData.streetAddress}
+            onChange={handleInputChange}
+            className="addForm__input"
+          />
+
+          <label className="addForm__label">City</label>
+          <input
+            type="text"
+            name="city"
+            placeholder="City"
+            value={formData.city}
+            onChange={handleInputChange}
+            className="addForm__input"
+          />
+
+          <label className="addForm__label">Country</label>
+          <input
+            type="text"
+            name="country"
+            placeholder="Country"
+            value={formData.country}
+            onChange={handleInputChange}
+            className="addForm__input"
+          />
+        </form>
+      </div>
+      </div>
+      <div className="addForm__tablet-form-contact-box">
         <div className="addForm__whd-border"></div>
         <div className="addForm__whd-container">
-          <h2 className="addForm__subheader">Warehouse Details</h2>
-
-          <form className="addForm__wh-form" onSubmit={handleSubmit} noValidate>
-            <label className="addForm__label">Warehouse Name</label>
-            <input
-              type="text"
-              name="warehouseName"
-              placeholder="Warehouse Name"
-              value={formData.warehouseName}
-              onChange={handleInputChange}
-              className="addForm__input"
-            />
-
-            <label className="addForm__label">Street Address</label>
-            <input
-              type="text"
-              name="streetAddress"
-              placeholder="Street Address"
-              value={formData.streetAddress}
-              onChange={handleInputChange}
-              className="addForm__input"
-            />
-
-            <label className="addForm__label">City</label>
-            <input
-              type="text"
-              name="city"
-              placeholder="City"
-              value={formData.city}
-              onChange={handleInputChange}
-              className="addForm__input"
-            />
-
-            <label className="addForm__label">Country</label>
-            <input
-              type="text"
-              name="country"
-              placeholder="Country"
-              value={formData.country}
-              onChange={handleInputChange}
-              className="addForm__input"
-            />
-            </form>
-        </div>
-        <div>
-          <div className="addForm__whd-border"></div>
-            <h2 className="addForm__subheader">Contact Details</h2>
+        <h2 className="addForm__subheader">Contact Details</h2>
 
         <form className="addForm__contact-form">
+          <label className="addForm__label">Contact Name</label>
+          <input
+            type="text"
+            name="contactName"
+            placeholder="Contact Name"
+            value={formData.contactName}
+            onChange={handleInputChange}
+            className="addForm__input"
+          />
 
-            <label className="addForm__label">Contact Name</label>
-            <input
-              type="text"
-              name="contactName"
-              placeholder="Contact Name"
-              value={formData.contactName}
-              onChange={handleInputChange}
-              className="addForm__input"
-            />
+          <label className="addForm__label">Position</label>
+          <input
+            type="text"
+            name="position"
+            placeholder="Position"
+            value={formData.position}
+            onChange={handleInputChange}
+            className="addForm__input"
+          />
 
-            <label className="addForm__label">Position</label>
-            <input
-              type="text"
-              name="position"
-              placeholder="Position"
-              value={formData.position}
-              onChange={handleInputChange}
-              className="addForm__input"
-            />
+          <label className="addForm__label">Phone Number</label>
+          <input
+            type="text"
+            name="phoneNumber"
+            placeholder="Phone Number"
+            value={formData.phoneNumber}
+            onChange={handleInputChange}
+            className="addForm__input"
+          />
 
-            <label className="addForm__label">Phone Number</label>
-            <input
-              type="text"
-              name="phoneNumber"
-              placeholder="Phone Number"
-              value={formData.phoneNumber}
-              onChange={handleInputChange}
-              className="addForm__input"
-            />
-
-            <label className="addForm__label">Email</label>
-            <input
-              type="text"
-              name="email"
-              placeholder="Email"
-              value={formData.email}
-              onChange={handleInputChange}
-              className="addForm__input"
-            />
-          </form>
+          <label className="addForm__label">Email</label>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleInputChange}
+            className="addForm__input"
+          />
+        </form>
         </div>
-        <div className="addForm__button-box">
-          <button className="btn__cancel">Cancel</button>
-          <button className="btn btn__submit" type="submit">
-            Save
-          </button>
-        </div>
-      </article>
-    </section>
-  );
+      </div>
+      </div>
+      <div className="addForm__button-box">
+        <button className="btn__cancel">Cancel</button>
+        <button className="btn btn__submit" type="submit">
+          Save
+        </button>
+      </div>
+    </article>
+);
 }
+
 
 export default EditWarehouse;
