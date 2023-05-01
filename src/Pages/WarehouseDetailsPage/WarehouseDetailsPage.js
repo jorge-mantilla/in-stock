e6d2d84cb3be4d2300efc12da77c2cc9449a551f
Brenda G <bren.gonzalez1926@gmail.com>
2023-04-30 23:09:17 -0400

    mid | Converted all PX to REM, as well as fixed Sass issue for Warehouse

--- src/Pages/WarehouseDetailsPage/WarehouseDetailsPage.js
+++ src/Pages/WarehouseDetailsPage/WarehouseDetailsPage.js
@@ -2,6 +2,10 @@ import WarehouseDetails from "../../components/WarehouseDetails/WarehouseDetails
 import { Link, useParams } from 'react-router-dom';
 import WarehouseInventoryList from "../../components/WarehouseInventoryList/WarehouseInventoryList";
 
+import BackIcon from '../../assets/Icons/arrow_back-24px.svg';
+
+import '../WarehouseDetailsPage/WarehouseDetailsPage.scss';
+
 function WarehouseDetailsPage(props) {
     const warehousesArray = props.warehousesArray
     const inventoriesArray = props.inventoriesArray
@@ -12,13 +16,15 @@ function WarehouseDetailsPage(props) {
     })
     return (
         <>
-            <Link to={`/Warehouse/`}>
-                <img alt="back arrow" />
-            </Link>
-            {warehousesArray.length > 0 &&
-                <WarehouseDetails specificWarehouse={specificWarehouse[0]} />
-            }
-            <WarehouseInventoryList inventoriesArray={inventoriesArray} specificWarehouse={specificWarehouse[0]} />
+            <section className="warehouse-inventory">
+                <Link className="inventory-item__link btn--return" to={`/Warehouse/`}>
+                    <img src={BackIcon} alt="Return Arrow Icon" />
+                </Link>
+                {warehousesArray.length > 0 &&
+                    <WarehouseDetails specificWarehouse={specificWarehouse[0]} />
+                }
+                <WarehouseInventoryList inventoriesArray={inventoriesArray} specificWarehouse={specificWarehouse[0]} />
+            </section>
         </>
     );
 }
